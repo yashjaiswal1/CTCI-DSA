ad31c4728d0c282ffe8cc6b17ca2eff905190044
Yash Jaiswal <yashjaiswal1@outlook.com>
2021-04-27 07:13:33 +0530

    Implemented problem #3.3

--- ds/stacks-and-queues/StackOfPlates.py
+++ ds/stacks-and-queues/StackOfPlates.py
@@ -1,4 +1,5 @@
 from collections import deque
+from typing import Deque
 
 
 class SetOfStacks:
@@ -6,13 +7,65 @@ class SetOfStacks:
     def __init__(self, capacity):
         self.set_of_stacks = []
         self.capacity = capacity
-        self.current_stack = None
-        self.current_stack_size = 0
+
+    def getLastStack(self):
+        return self.set_of_stacks[-1] if self.set_of_stacks != [] else Deque()
 
     def push(self, data):
-        if (self.current_stack_size == 0):
-            self.current_stack = deque()
-            self.set_of_stacks.append(self.current_stack)
-        elif (self.current_stack_size == self.capacity):
-            # generate a new stack
-        self.current_stack.append(data)
+        current_stack = self.getLastStack()
+        is_stack_full = len(current_stack) == self.capacity
+        if (self.set_of_stacks != [] and is_stack_full != True):
+            current_stack.append(data)
+        else:
+            current_stack = Deque()
+            self.set_of_stacks.append(current_stack)
+            current_stack = self.getLastStack()  # remove this line
+            current_stack.append(data)
+
+    def pop(self):
+        current_stack = self.getLastStack()
+
+        if (self.set_of_stacks == []):
+            print("Stack Underflow!")
+            return
+        popped_element = current_stack.pop()
+
+        is_stack_empty = len(current_stack) == 0
+        if is_stack_empty:
+            self.set_of_stacks.pop()
+
+        return popped_element
+
+
+StackOfPlates = SetOfStacks(3)
+StackOfPlates.pop()
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.push(10)
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.push(20)
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.push(30)
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.push(40)
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.push(50)
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.push(60)
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.push(70)
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.pop()
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.pop()
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.pop()
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.pop()
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.pop()
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.pop()
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.pop()
+print(StackOfPlates.set_of_stacks)
+StackOfPlates.pop()
